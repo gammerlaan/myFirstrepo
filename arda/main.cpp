de922262308cded66835d3cff556ee4479aad26a
gammerlaan <gammerlaan@yahoo.com>
2017-01-21 13:00:36 +0100

    Played around with the "try, throw, catch" routines..

--- arda/main.cpp
+++ arda/main.cpp
@@ -2,20 +2,23 @@
 using namespace std;
 
 const int SUCCESS{0};
-const int FAILURE{13};
+const int FAST_EXIT{13};
 
 int main()
-{
-    int answer{1};
+	{
+    	int answer{1};
 
-    while (answer!=0){
-    try{
-    cout << "You membah? Yeah I membah!" << endl;
-    cin >> answer;
-    if (answer==0){throw FAILURE;}
-    }
-    catch(int x){cout << "I no longah membah " << x << endl;
-    return x;}
-    }
-    return SUCCESS;
-}
+    	while (answer!=0){
+    		try{
+    			cout << "You membah? Yeah I membah!" << endl;
+    			cin >> answer;
+    			if (answer==0){throw FAST_EXIT;}
+    		}
+    		catch(int x){cout << "I no longah membah " << x << endl;
+            return x;
+            }
+    	}
+    	return SUCCESS;
+	}
+
+
